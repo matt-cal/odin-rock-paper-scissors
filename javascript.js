caddd079d7b637c1d1d967d8db9cae7c34e0309f
Matthew Callister <116524474+matt-cal@users.noreply.github.com>
2022-11-13 13:50:39 -0500

    Add function playRound and helper functions
    playRound simulates a round. checkWin and capitalize added for use in
    playRound.

--- javascript.js
+++ javascript.js
@@ -1,11 +1,48 @@
 function getComputerChoice(){
+    /*
+    randomly returns either "Rock", "Paper", or "Scissors"
+    */
     const dict = {
         0: "Rock",
         1: "Paper",
         2: "Scissors"
     };
+    // random int between 0 and 2 inclusive
     choice = Math.floor(Math.random() * 3);
     return dict[choice];
 }
 
-console.log("hello");
+function capitalize(string) {
+    /*
+    capitalizes first character in string, makes all others lowercase
+    returns new string
+    */
+    string = string.toLowerCase();
+    return string.replace(string.charAt(0), string.charAt(0).toUpperCase());
+}
+
+function checkWin(playerSelection, computerSelection) {
+    /*
+    returns true if player beats computer, false otherwise
+    */
+    if ((playerSelection=="Rock" && computerSelection=="Scissors") || 
+    (playerSelection=="Paper" && computerSelection=="Rock") ||
+    (playerSelection=="Scissors" && computerSelection=="Paper")) {
+        return true;
+    }
+    return false;
+}
+
+function playRound(playerSelection, computerSelection) {
+    /*
+    returns string describing result of a round based on player and computer selections
+    */
+    playerSelection = capitalize(playerSelection);
+    if (playerSelection == computerSelection) {
+        return `Tie! You both picked ${playerSelection}!`;
+    } else if (checkWin(playerSelection, computerSelection)){
+        return `You Win! ${playerSelection} beats ${computerSelection}`;
+    } else {
+        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    }
+}
